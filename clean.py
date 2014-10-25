990b6b95058d2956ac9c6ffb07ae6206ad61eaac
chihungfei <chihungfei@163.com>
2014-10-26 04:18:23 +0800

    Create clean.py

---
+++ clean.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import os, string, sys, re
+
+filePath = os.path.split( os.path.realpath( sys.argv[0] ) )[0]
+os.chdir(filePath)
+
+
+def fileit( path ):
+    a = []
+    for root, dirs, files in os.walk( path ):
+        for fn in files:
+            if fn[-3:] == 'txt':
+                p = root+'/'+fn
+                a.append(p)
+                
+    return a
+            
+
+files = fileit('.')
+newf = []
+for each in files:
+    newf.append(each[2:])
+    
+files = newf
+print files
+
+for each in files:
+    out = each
+    lines = open(each).readlines()
+    fp = open(each,'w')  #打开你要写得文件pp2.txt
+    pt = re.compile(r'<.+?>')
+    for s in lines:
+       fp.write(re.sub(pt, '', s))
+    fp.close()  # 关闭文件
+
+    
